703c79d253b16f7c55494cddd2bc083f7e8012a4
bbbales2 <bbbales2@gmail.com>
2017-12-12 15:45:27 -0800

    Changesss

--- process_lm.R
+++ process_lm.R
@@ -4,49 +4,49 @@ library(ggplot2)
 library(readr)
 library(rstanarm)
 library(shinystan)
-library(purrrlyr)
-library(plotly)
-library(ggthemes)
+#library(purrrlyr)
 
+# Read in all the list of files to process
 df = tbl_df(read_csv('/home/bbales2/CreepInfo/creep.csv')) %>%
   select(-iminf, -imaxf) %>%
   mutate(csv = paste(file, ".csv", sep = ""))
 
-getSlope = function(row) {
-  data = read_csv(row['csv'])
+getSlope = function(filename) {
+  data = read_csv(filename)
   
   fit = lm(stress ~ time, data = data)
   
   c(coefficients(fit)[['time']], sqrt(vcov(fit)[['time', 'time']]))
 }
 
-getSlope(as.matrix(df)[1,])
-
-a = df %>% apply(1, getSlope) %>% t()
+# For each csv file, read in the data, get a linear fit, and put it in a dataframe (df2)
+a = df %>%
+  pull(csv) %>%
+  map(getSlope) %>%
+  unlist() %>%
+  matrix(ncol = 2, byrow = TRUE)
 colnames(a)[1:2] <- c("slopes", "slope_std_err")
-
-df = bind_cols(df, tbl_df(a))
-
-df2 = df %>%
+df2 = bind_cols(df, tbl_df(a)) %>%
   select(thickness, stress, slopes, slope_std_err) %>%
   mutate(thickness_id = as.integer(as.factor(thickness))) %>%
   mutate(lmus = log(slopes))
 
+# This prints the slopes and errors in slopes for all combos of thicknesses and stress
 df2 %>% print(n = 40)
 
-data = list(L = nrow(df),
+data = list(L = nrow(df2),
             T = max(df2$thickness_id),
             labels = df2$thickness_id,
             stress = df2$stress,
             thickness = df2$thickness,
             mus = df2$lmus)
 
+# Run the LMP fit
 fit = stan("/home/bbales2/CreepInfo/lumped.stan", data = data, cores = 4)
-
-#launch_shinystan(fit)
 
 s = extract(fit)
 
+# Get all the data out and plot it!
 dfp = left_join(df2 %>% mutate(idx = 1),
                 as_tibble(list(aa = s$a, bb = s$b, cc = s$c)) %>% mutate(idx = 1)) %>%
   select(-idx) %>%
@@ -100,16 +100,6 @@ df3 %>% group_by(thickness, stress) %>% sample_n(100) %>% ggplot(aes(stress, exp
   geom_ribbon(data = df4, aes(stress, ymin = exp(mean_mumus_m2sd), ymax = exp(mean_mumus_p2sd)), fill = "dodgerblue1", alpha = 0.2) +
   geom_point(data = df4, aes(stress, exp(lmus)), color = "violetred2") +
   scale_y_log10() +
-  facet_wrap(~ thickness, labeller = "label_both") + theme_excel()# + scale_color_excel()
+  facet_wrap(~ thickness, labeller = "label_both")# + scale_color_excel()
 
 df4 %>% mutate(ln_slopes = lmus) %>% select(-lmus_calc, -thickness_id, -lmus) %>% print(n = 40)
-
-colnames(s$sigma) <- 1:4
-
-df4 %>% group_by(thickness, stress) %>%
-
-as.tibble(s$sigma[,]) %>%
-  gather("row", "sigmas", 1:4) %>%
-  mutate(row = as.integer(row)) %>%
-  left_join()
-
